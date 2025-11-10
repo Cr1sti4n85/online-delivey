5e35930502027c05f8b1d72c9fe310ca99c79a7b
Cristian Perez <cperezlecaros@gmail.com>
2025-11-10 14:02:07 -0300

    Initial commit

---
+++ src/test/java/com/soracel/onlinemenu/OnlineMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soracel.onlinemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
